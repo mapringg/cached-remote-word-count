3861b4f7f60a38869bf2a14aae81dd75673a6c87
Sorawit Kongnurat <trainer.mapring@gmail.com>
2020-01-11 17:30:33 +0700

    fixing concurrent user retrieving data from cache

---
+++ helpers.js
@@ -0,0 +1,51 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+exports.responseBasedOnType = (url, res, accept, wordCount, topTenWords) => {
+  if (accept === 'application/json') {
+    // console.log('fetched')
+    return res.send({ wordCount, topTenWords })
+  } else if (accept === 'text/plain') {
+    res.set('Content-Type', 'text/plain')
+    request(
+      `http://localhost:3000/wc?target=${url}`,
+      (error, response, body) => {
+        const $ = cheerio.load(body)
+        const words = $('body').text()
+        // console.log('fetched')
+        return res.send(words)
+      }
+    )
+  } else {
+    // console.log('fetched')
+    return res.render('index', {
+      url,
+      wordCount,
+      topTenWords
+    })
+  }
+}
+
+exports.extractTopTenWords = words => {
+  const topTenWords = {}
+  for (word of words) {
+    if (!topTenWords[word]) {
+      topTenWords[word] = 1
+    }
+    topTenWords[word]++
+  }
+
+  let sortable = []
+  for (word in topTenWords) {
+    sortable.push([word, topTenWords[word]])
+  }
+  sortable.sort((a, b) => {
+    return b[1] - a[1]
+  })
+  sortable = sortable.slice(0, 10)
+  let objSorted = {}
+  sortable.forEach(item => {
+    objSorted[item[0]] = item[1]
+  })
+  return objSorted
+}
---
+++ tests/index.test.js
@@ -0,0 +1,119 @@
+const app = require('../index') // Link to your server file
+const request = require('supertest')
+
+describe('GET wc?target=https://https://stackoverflow.com', () => {
+  const url = '/wc'
+  const target = 'https://stackoverflow.com'
+
+  it('server should be able to support concurrency of up to 5 users and serve cache data after the data of the first user have been stored', async () => {
+    const firstUserCal = request(app)
+      .get(url)
+      .query({ target })
+    const secondUserCall = request(app)
+      .get(url)
+      .query({ target })
+    // const thirdUserCall = request(app)
+    //   .get(url)
+    //   .query({ target })
+    // const fourthUserCall = request(app)
+    //   .get(url)
+    //   .query({ target })
+    // const fifthUserCall = request(app)
+    //   .get(url)
+    //   .query({ target })
+    const [
+      firstUserRes,
+      secondUserRes
+      // thirdUserRes,
+      // fourthUserRes,
+      // fifthUserRes
+    ] = await Promise.all([
+      firstUserCal,
+      secondUserCall
+      // thirdUserCall,
+      // fourthUserCall,
+      // fifthUserCall
+    ])
+    console.log(firstUserRes.header['cache-data'])
+    console.log(secondUserRes.header['cache-data'])
+    // console.log(thirdUserRes.header['cache-data'])
+    // console.log(fourthUserRes.header['cache-data'])
+    // console.log(fifthUserRes.header['cache-data'])
+  })
+})
+
+// describe('GET /wc?target=https://www.ubuntu.com', () => {
+//   const url = '/wc'
+//   const target = 'https://www.ubuntu.com'
+
+//   it('server should not return cache data if this is the first call', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({ target })
+//     const cacheData = res.header['cache-data']
+//     expect(cacheData).toBe('false')
+//   })
+
+//   it('server should return cache data if this is the second call', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({ target })
+//     const cacheData = res.header['cache-data']
+//     expect(cacheData).toBe('true')
+//   })
+
+//   it('server should not return cache data if user specify force=true', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({
+//         target,
+//         force: 'true'
+//       })
+//     const cacheData = res.header['cache-data']
+//     expect(cacheData).toBe('false')
+//   })
+
+//   it('server should return cache data if user specify force=false and the data exists', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({
+//         target,
+//         force: 'false'
+//       })
+//     const cacheData = res.header['cache-data']
+//     expect(cacheData).toBe('true')
+//   })
+// })
+
+// describe('GET /wc?target=https://www.archlinux.org', () => {
+//   const url = '/wc'
+//   const target = 'https://www.archlinux.org'
+
+//   it('content-type should default to text/html when not specify', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({ target })
+//     const contentType = res.header['content-type']
+//     expect(contentType.slice(0, contentType.indexOf(';'))).toBe('text/html')
+//   })
+
+//   it('content-type should default to application/json when specified', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({ target })
+//       .set('Accept', 'application/json')
+//     const contentType = res.header['content-type']
+//     expect(contentType.slice(0, contentType.indexOf(';'))).toBe(
+//       'application/json'
+//     )
+//   })
+
+//   it('content-type should default to text/plain when specified', async () => {
+//     const res = await request(app)
+//       .get(url)
+//       .query({ target })
+//       .set('Accept', 'text/plain')
+//     const contentType = res.header['content-type']
+//     expect(contentType.slice(0, contentType.indexOf(';'))).toBe('text/plain')
+//   })
+// })
