6c239b929e295f38f9d78a5b68fc12039ac553f1
Sorawit Kongnurat <trainer.mapring@gmail.com>
2020-01-16 00:09:30 +0700

    add test for concurrent hitting different route and etag

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,57 @@
 const app = require('../index') // Link to your server file
 const request = require('supertest')
 
-describe('GET wc?target=https://https://stackoverflow.com', () => {
-  const url = '/wc'
-  const target = 'https://stackoverflow.com'
+const url = '/wc'
 
-  it('server should be able to support concurrency of up to 5 users and serve cache data after the data of the first user have been stored', async () => {
-    const firstUserCal = request(app)
+describe('GET wc?target (multiple targets)', () => {
+  const target1 = 'https://www.google.com'
+  const target2 = 'https://www.wikipedia.org/'
+  const target3 = 'https://www.mozilla.org/'
+  const target4 = 'https://code.visualstudio.com/'
+  const target5 = 'https://www.sublimetext.com/'
+
+  it('server should not wait for the first GET, if hitting a different route concurrently', async () => {
+    const firstUserCall = request(app)
+      .get(url)
+      .query({ target: target1 })
+    const secondUserCall = request(app)
+      .get(url)
+      .query({ target: target2 })
+    const thirdUserCall = request(app)
+      .get(url)
+      .query({ target: target3 })
+    const fourthUserCall = request(app)
+      .get(url)
+      .query({ target: target4 })
+    const fifthUserCall = request(app)
+      .get(url)
+      .query({ target: target5 })
+    const [
+      firstUserRes,
+      secondUserRes,
+      thirdUserRes,
+      fourthUserRes,
+      fifthUserRes
+    ] = await Promise.all([
+      firstUserCall,
+      secondUserCall,
+      thirdUserCall,
+      fourthUserCall,
+      fifthUserCall
+    ])
+    expect(firstUserRes.header['cache-data']).toBe('false')
+    expect(secondUserRes.header['cache-data']).toBe('false')
+    expect(thirdUserRes.header['cache-data']).toBe('false')
+    expect(fourthUserRes.header['cache-data']).toBe('false')
+    expect(fifthUserRes.header['cache-data']).toBe('false')
+  }, 20000)
+})
+
+describe('GET wc?target=https://curl.haxx.se/', () => {
+  const target = 'https://curl.haxx.se/'
+
+  it('server should return cache from first GET, if hitting the same route concurrently', async () => {
+    const firstUserCall = request(app)
       .get(url)
       .query({ target })
     const secondUserCall = request(app)
@@ -28,7 +73,7 @@ describe('GET wc?target=https://https://stackoverflow.com', () => {
       fourthUserRes,
       fifthUserRes
     ] = await Promise.all([
-      firstUserCal,
+      firstUserCall,
       secondUserCall,
       thirdUserCall,
       fourthUserCall,
@@ -42,9 +87,8 @@ describe('GET wc?target=https://https://stackoverflow.com', () => {
   })
 })
 
-describe('GET /wc?target=https://curl.haxx.se/', () => {
-  const url = '/wc'
-  const target = 'https://curl.haxx.se/'
+describe('GET /wc?target=https://www.stackoverflow.com', () => {
+  const target = 'https://www.stackoverflow.com'
 
   it('server should not return cache data if this is the first call', async () => {
     const res = await request(app)
@@ -86,8 +130,21 @@ describe('GET /wc?target=https://curl.haxx.se/', () => {
 })
 
 describe('GET /wc?target=https://www.archlinux.org', () => {
-  const url = '/wc'
   const target = 'https://www.archlinux.org'
+
+  it('etag-changed should be set to new for request that is not in cache', async () => {
+    const res = await request(app)
+      .get(url)
+      .query({ target })
+    expect(res.header['etag-changed']).toBe('new')
+  })
+
+  it('etag-changed should be set to false for request that is in cache, but etag remained the same', async () => {
+    const res = await request(app)
+      .get(url)
+      .query({ target })
+    expect(res.header['etag-changed']).toBe('false')
+  })
 
   it('content-type should default to text/html when not specify', async () => {
     const res = await request(app)
